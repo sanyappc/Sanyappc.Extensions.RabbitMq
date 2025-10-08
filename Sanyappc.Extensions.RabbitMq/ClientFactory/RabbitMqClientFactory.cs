bd04f0e9da680c224404d5ec098efa3a88361d3d
i.novoseltsev <i.novoseltsev@nt.ru>
2025-10-08 15:36:07 +0300

    Minor refactoring of client factory

--- Sanyappc.Extensions.RabbitMq/ClientFactory/RabbitMqClientFactory.cs
+++ Sanyappc.Extensions.RabbitMq/ClientFactory/RabbitMqClientFactory.cs
@@ -15,14 +15,9 @@ namespace Sanyappc.Extensions.RabbitMq
         protected readonly ILoggerFactory loggerFactory = loggerFactory;
         protected readonly IRabbitMqChannelFactory rabbitMqChannelFactory = rabbitMqChannelFactory;
 
-        public IRabbitMqConsumer BuildConsumer(string connectionName = null)
+        public IRabbitMqConsumer BuildConsumer(string? connectionName = null)
         {
-            RabbitMqConnectionSettings? rabbitOptions;
-
-            if (connectionName == null)
-                rabbitOptions = options.Value.Connection;
-            else if (!options.Value.Connections.TryGetValue(connectionName, out rabbitOptions))
-                throw new KeyNotFoundException($"No Connection config named \"{connectionName}\"");
+            RabbitMqConnectionSettings rabbitOptions = GetConnection(connectionName);
 
             return new RabbitMqConsumer(
                 loggerFactory.CreateLogger<RabbitMqConsumer>(),
@@ -32,20 +27,27 @@ namespace Sanyappc.Extensions.RabbitMq
                 rabbitOptions!.QueueName);
         }
 
-        public IRabbitMqPublisher BuildPublisher(string connectionName = null)
+        public IRabbitMqPublisher BuildPublisher(string? connectionName = null)
         {
-            RabbitMqConnectionSettings? rabbitOptions;
-
-            if (connectionName == null)
-                rabbitOptions = options.Value.Connection;
-            else if (!options.Value.Connections.TryGetValue(connectionName, out rabbitOptions))
-                throw new KeyNotFoundException($"No Connection config named \"{connectionName}\"");
+            RabbitMqConnectionSettings rabbitOptions = GetConnection(connectionName);
 
             return new RabbitMqPublisher(
                 loggerFactory.CreateLogger<RabbitMqPublisher>(),
                 rabbitMqChannelFactory,
                 connectionName!,
                 rabbitOptions!.QueueName);
+        }
+
+        private RabbitMqConnectionSettings GetConnection(string? connectionName)
+        {
+            RabbitMqConnectionSettings? rabbitOptions;
+
+            if (connectionName == null)
+                rabbitOptions = options.Value.Connection;
+            else if (!options.Value.Connections.TryGetValue(connectionName, out rabbitOptions))
+                throw new KeyNotFoundException($"No connection config named \"{connectionName}\"");
+
+            return rabbitOptions!;
         }
     }
 }
